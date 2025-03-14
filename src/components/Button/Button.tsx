4697348a4862d978157b5cf997cc83672bf06eb7
Alina Goremykina <a.goremykina98@gmail.com>
2025-03-14 16:24:12 +0100

    Add storybook

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -3,12 +3,14 @@ import React from 'react';
 export interface ButtonProps {
     // color?: string;
     // variant?: string;
-    // size?: string | number;
-    className?: string;
+    // size?: 'small' | 'medium' | 'large';
+    onClick?: () => void;
+    label: string;
+    // className?: string;
 }
 
-const Button: React.FC<ButtonProps> = ({ className }) => {
-    return <button className={className}>click</button>;
+const Button: React.FC<ButtonProps> = ({ label, onClick }) => {
+    return <button onClick={onClick}>{label}</button>;
 };
 
 export default Button;
