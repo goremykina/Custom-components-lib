94a5fb113ae0cde9021ec0c7d9b3321e54400885
Alina Goremykina <a.goremykina98@gmail.com>
2025-03-31 15:30:42 +0200

    Import all components

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
 export * from './components/Button/Button';
+export * from './components/Checkbox/Checkbox';
+export * from './components/Switch/Switch';
+export * from './components/Select/Select';
+export * from './components/Modal/Modal';
+export * from './components/TextField/TextField';
+
